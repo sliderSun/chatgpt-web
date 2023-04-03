bfcd4101dd51ac548b9c0376369f9c6fc609ae8e
bobicoli <bobi59145@gmail.com>
2023-04-03 23:25:11 +0800

    perf: 调整光标位置 同 15f3aac

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -6,7 +6,6 @@ import mila from 'markdown-it-link-attributes'
 import hljs from 'highlight.js'
 import { useBasicLayout } from '@/hooks/useBasicLayout'
 import { t } from '@/locales'
-
 interface Props {
   inversion?: boolean
   error?: boolean
@@ -14,13 +13,9 @@ interface Props {
   loading?: boolean
   asRawText?: boolean
 }
-
 const props = defineProps<Props>()
-
 const { isMobile } = useBasicLayout()
-
 const textRef = ref<HTMLElement>()
-
 const mdi = new MarkdownIt({
   linkify: true,
   highlight(code, language) {
@@ -32,51 +27,42 @@ const mdi = new MarkdownIt({
     return highlightBlock(hljs.highlightAuto(code).value, '')
   },
 })
-
 mdi.use(mila, { attrs: { target: '_blank', rel: 'noopener' } })
 mdi.use(mdKatex, { blockClass: 'katexmath-block rounded-md p-[10px]', errorColor: ' #cc0000' })
-
 const wrapClass = computed(() => {
   return [
     'text-wrap',
     'min-w-[20px]',
     'rounded-md',
-    isMobile.value ? 'p-2' : 'p-3',
+    isMobile.value ? 'p-2' : 'px-3 py-2',
     props.inversion ? 'bg-[#d2f9d1]' : 'bg-[#f4f6f8]',
     props.inversion ? 'dark:bg-[#a1dc95]' : 'dark:bg-[#1e1e20]',
     props.inversion ? 'message-request' : 'message-reply',
     { 'text-red-500': props.error },
   ]
 })
-
 const text = computed(() => {
   const value = props.text ?? ''
   if (!props.asRawText)
     return mdi.render(value)
   return value
 })
-
 function highlightBlock(str: string, lang?: string) {
   return `<pre class="code-block-wrapper"><div class="code-block-header"><span class="code-block-header__lang">${lang}</span><span class="code-block-header__copy">${t('chat.copyCode')}</span></div><code class="hljs code-block-body ${lang}">${str}</code></pre>`
 }
-
 defineExpose({ textRef })
 </script>
 
 <template>
   <div class="text-black" :class="wrapClass">
-    <template v-if="loading">
-      <span class="dark:text-white w-[4px] h-[20px] block animate-blink" />
-    </template>
-    <template v-else>
-      <div ref="textRef" class="leading-relaxed break-words">
-        <div v-if="!inversion">
-          <div v-if="!asRawText" class="markdown-body" v-html="text" />
-          <div v-else class="raw-text" v-text="text" />
-        </div>
-        <div v-else class="whitespace-pre-wrap" v-text="text" />
+    <div ref="textRef" class="leading-relaxed break-words">
+      <div v-if="!inversion" class="flex items-end">
+        <div v-if="!asRawText" class="w-full markdown-body" v-html="text" />
+        <div v-else class="w-full whitespace-pre-wrap" v-text="text" />
+        <span v-if="loading" class="dark:text-white w-[4px] h-[20px] block animate-blink" />
       </div>
-    </template>
+      <div v-else class="whitespace-pre-wrap" v-text="text" />
+    </div>
   </div>
 </template>
 
